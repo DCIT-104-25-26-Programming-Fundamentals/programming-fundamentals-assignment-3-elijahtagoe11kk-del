c98cbf0b9da5eb10331b00316d92adacf17473d9
kobby jr <elijahtagoe11kk@gmail.com>
2026-07-30 21:54:16 +0000

    work 1

--- assignment_01_prime_checker.js
+++ assignment_01_prime_checker.js
@@ -43,3 +43,32 @@
 // =============================================================================
 
 
+const readlineSync = require("readline-sync");
+
+// Function to check if a number is prime
+function isPrime(num) {
+    if (num < 2) {
+        return false;
+    }
+
+    for (let i = 2; i * i <= num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Main function
+function main() {
+    const number = readlineSync.questionInt("Enter a number: ");
+
+    if (isPrime(number)) {
+        console.log(number + " is a prime number.");
+    } else {
+        console.log(number + " is NOT a prime number.");
+    }
+}
+
+main();
